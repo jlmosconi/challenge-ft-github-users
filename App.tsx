145acd5d482563f4f4d093dac61ec87cf5d9ecfe
José Luis Mosconi <jmosconi@fintech-consulting.services>
2025-07-10 18:36:58 -0300

    feat(config): add iOS schemes, Android flavors and .env support for multi-environment builds

--- App.tsx
+++ App.tsx
@@ -1,10 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {StatusBar, StyleSheet, Text, useColorScheme, View} from 'react-native';
+import CONFIG from '@config/environment/current';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -12,6 +7,14 @@ function App() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text
+        style={{
+          fontSize: 20,
+          textAlign: 'center',
+          marginTop: 150,
+        }}>
+        {CONFIG.MODE} - {CONFIG.API.BASE_URL}
+      </Text>
     </View>
   );
 }
---
+++ scripts/firebaseFilesScript.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+echo "Platform : " $2
+echo "Command found : " $1
+echo "Replacing google service file..."
+
+# ANDROID
+if [ $2 = "android" ]
+then
+    if [[ $1 == *"Development"* ]]; then
+        echo "Setting Android Development"
+        cp -f ../../app/config/environment/development/google-services.json ./google-services.json && echo "cp command success" || echo "cp command failed"
+    elif [[ $1 == *"Production"* ]]; then
+        echo "Setting Android Production"
+        cp -f ../../app/config/environment/production/google-services.json ./google-services.json && echo "cp command success" || echo "cp command failed"
+    else
+        echo "Setting Android Development by default"
+        cp -f ../../app/config/environment/development/google-services.json ./google-services.json && echo "cp command success" || echo "cp command failed"
+    fi
+fi
+
+# IOS
+if [ $2 = "ios" ]
+then
+    if [[ $1 == *"dev"* ]]; then
+        echo "Setting iOS Development"
+        cp -f ../app/config/environment/development/GoogleService-Info.plist ./Blank/GoogleService-Info.plist && echo "cp command success" || echo "cp command failed"
+    elif [[ $1 != *"prod"* ]]; then
+        echo "Setting iOS Production"
+        cp -f ../app/config/environment/production/GoogleService-Info.plist ./Blank/GoogleService-Info.plist && echo "cp command success" || echo "cp command failed"
+    else
+        echo "Setting iOS Development by default"
+        cp -f ../app/config/environment/development/GoogleService-Info.plist ./Blank/GoogleService-Info.plist && echo "cp command success" || echo "cp command failed"
+    fi
+fi
+
+echo "Replacing google service file finished."
