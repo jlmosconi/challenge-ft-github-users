190e6ed02b2b227f4040bbf28c73c32294868bcf
José Luis Mosconi <jlmosconi@gmail.com>
2025-07-12 12:02:55 -0300

    refactor: enhance FavoritesScreen by integrating toggleFavorite logic and optimizing favorites rendering

--- app/screens/Favorites/index.tsx
+++ app/screens/Favorites/index.tsx
@@ -1,15 +1,46 @@
-import {type FC} from 'react';
+import {useCallback, useMemo, type FC} from 'react';
 import SafeArea from '@components/SafeArea';
-import {Text} from 'react-native';
-import {selectFavorites} from '@store/slices/favorites';
-import {useAppSelector} from '@store/hooks';
+import {selectFavorites, toggleFavorite} from '@store/slices/favorites';
+import {useAppDispatch, useAppSelector} from '@store/hooks';
+import InfiniteScrollList from '@components/InfiniteScrollList';
+import UserItem from '@components/Users/Item';
+import {FavoriteUser} from '@store/slices/favorites/types';
 
 const FavoritesScreen: FC = () => {
   const favorites = useAppSelector(selectFavorites);
-  console.log('Favorites:', favorites);
+  const dispatch = useAppDispatch();
+
+  // transform the favorites object into an array for rendering, and memoize it for performance
+  // this avoids unnecessary recalculations on every render
+  const favoritesList = useMemo(() => Object.values(favorites), [favorites]);
+
+  const handleOnFavoritePress = useCallback(
+    (user: FavoriteUser) => {
+      dispatch(toggleFavorite(user));
+    },
+    [dispatch],
+  );
+
   return (
     <SafeArea>
-      <Text>Favorites</Text>
+      <InfiniteScrollList
+        data={favoritesList}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item: {id, login, avatar_url}}) => (
+          <UserItem
+            login={login}
+            avatar_url={avatar_url}
+            handleOnFavorite={() =>
+              handleOnFavoritePress({
+                id,
+                login,
+                avatar_url,
+              })
+            }
+            isFavorite={!!favorites[id]}
+          />
+        )}
+      />
     </SafeArea>
   );
 };
