6a2e5818ddff3a09a1834e12985efd6795195e7f
José Luis Mosconi <jlmosconi@gmail.com>
2025-07-13 11:12:34 -0300

    refactor: add type definition for route props in UserScreen component

--- app/screens/User/index.tsx
+++ app/screens/User/index.tsx
@@ -1,7 +1,12 @@
 import {type FC} from 'react';
+import {type RouteProp} from '@react-navigation/native';
+import {MainScreen, MainStack} from '@navigators/screenRoutes';
 import {Text, View} from 'react-native';
 
-const UserScreen: FC = () => {
+type Props = {route: RouteProp<MainStack, MainScreen.User>};
+
+const UserScreen: FC<Props> = ({route}) => {
+  console.log('UserScreen route params:', route.params);
   return (
     <View>
       <Text>UserScreen</Text>
