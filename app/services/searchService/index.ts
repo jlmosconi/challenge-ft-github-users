b7e3ef8e85bf520496da0d181a5d590b3891e918
José Luis Mosconi <jlmosconi@gmail.com>
2025-07-23 14:24:59 -0300

    fix(api): remove CONFIG dependency and use static baseUrl for user and search services

--- app/services/searchService/index.ts
+++ app/services/searchService/index.ts
@@ -1,9 +1,8 @@
 import {api} from '@services/api';
-import CONFIG from '@config/environment/current';
 import type {ErrorResponse} from '@services/api/types';
 import type {IUserSearchResponse} from './types';
 
-const baseUrl = `${CONFIG.API.BASE_URL}/search`;
+const baseUrl = `/search`;
 
 const searchUsers = (query: string, limit = 10) =>
   api.get<IUserSearchResponse, ErrorResponse>(`${baseUrl}/users`, {q: query, per_page: limit});
--- app/services/usersService/index.ts
+++ app/services/usersService/index.ts
@@ -1,9 +1,8 @@
 import {api} from '@services/api';
-import CONFIG from '@config/environment/current';
 import type {ErrorResponse} from '@services/api/types';
 import type {IUserListResponse, IUserResponse} from './types';
 
-const baseUrl = `${CONFIG.API.BASE_URL}/users`;
+const baseUrl = `/users`;
 
 const getUserList = (since: number, limit: number) =>
   api.get<IUserListResponse[], ErrorResponse>(`${baseUrl}`, {
