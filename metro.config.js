215b6502b8d256a5cb140be218138a8da78e71a3
José Luis Mosconi <jlmosconi@gmail.com>
2025-07-23 17:24:18 -0300

    refactor(metro): clean up commented code and improve configuration clarity

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,4 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-// const exclusionList = require('metro-config/src/defaults/exclusionList');
 const path = require('path');
 const withStorybook = require('@storybook/react-native/metro/withStorybook');
 const defaultConfig = getDefaultConfig(__dirname);
@@ -18,27 +17,10 @@ const config = {
   resolver: {
     assetExts: assetExts.filter(ext => ext !== 'svg'),
     sourceExts: [...sourceExts, 'svg'],
-    // blacklistRE: exclusionList([
-    //   /ios\/build\/SourcePackages\/checkouts\/grpc-ios\/native_src\/examples\/.*/,
-    // ]),
-    // resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
   },
 };
 
-// module.exports = mergeConfig(defaultConfig, config);
-
 module.exports = withStorybook(mergeConfig(defaultConfig, config), {
-  // Set to false to remove storybook specific options
-  // you can also use a env variable to set this
   enabled: true,
-  // Path to your storybook config
   configPath: path.resolve(__dirname, './.rnstorybook'),
-  // note that this is the default so you can the config path blank if you use .rnstorybook
-
-  // Optional websockets configuration
-  // Starts a websocket server on the specified port and host on metro start
-  // websockets: {
-  //   port: 7007,
-  //   host: 'localhost',
-  // },
 });
