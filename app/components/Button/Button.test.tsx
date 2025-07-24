a033092b5229d92ce9d8715411ea5e1b4d21d971
José Luis Mosconi <jlmosconi@gmail.com>
2025-07-24 16:53:36 -0300

    test(Button): fix button title in test case and update pre-commit hook

--- app/components/Button/Button.test.tsx
+++ app/components/Button/Button.test.tsx
@@ -11,7 +11,7 @@ describe('Button', () => {
 
   it('renders the button with the given title', () => {
     const {getByText} = renderWithTheme(<Button title="Click me" />);
-    expect(getByText('Click me23')).toBeTruthy();
+    expect(getByText('Click me233')).toBeTruthy();
   });
 
   it('calls onPress when pressed', () => {
