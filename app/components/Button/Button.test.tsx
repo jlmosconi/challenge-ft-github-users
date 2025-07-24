92ff381ef241cc10c577e8425d92397f10e29bee
José Luis Mosconi <jlmosconi@gmail.com>
2025-07-24 16:34:07 -0300

    test(Button): update test to check for modified button title

--- app/components/Button/Button.test.tsx
+++ app/components/Button/Button.test.tsx
@@ -11,7 +11,7 @@ describe('Button', () => {
 
   it('renders the button with the given title', () => {
     const {getByText} = renderWithTheme(<Button title="Click me" />);
-    expect(getByText('Click me')).toBeTruthy();
+    expect(getByText('Click me2')).toBeTruthy();
   });
 
   it('calls onPress when pressed', () => {
